7a6776739063da604894ad34ef24c07683c815ee
trungtin1808 <154118963+trungtin1808@users.noreply.github.com>
2023-12-21 08:12:10 +0700

    Update HoaDonDAO.java

--- DAO/HoaDonDAO.java
+++ DAO/HoaDonDAO.java
@@ -9,47 +9,48 @@ package DAO;
  * @author Admin
  */
 import java.sql.*;
-import Object.HoaDon;
+import Objects.HoaDon;
+import java.util.List;
+import Objects.HoaDon;
 import java.util.ArrayList;
+
 
 public class HoaDonDAO {
 
     public static Connection conn = Connect.getConnect();
+    public static List<Integer> getlistmADOCGIA(){
+        List<Integer> list=new ArrayList<>();
+        String sql="select MADOCGIA from DOCGIA";
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
+             ResultSet rs = stm.executeQuery();
+             while(rs.next()){
+                 list.add(rs.getInt("MADOCGIA"));
+                 
+             }   
+                
+            } catch (SQLException e) {
+                throw new ArithmeticException(e.getMessage());
+            }
+        
+        return list;
+    }
 
     public static void ThemHoaDon(HoaDon hd) {
         String sql="{call THEMHOADON(?, ?)}";
         try (CallableStatement stmt = conn.prepareCall(sql);) {
-                stmt.setInt(1,hd.getmAPHIEUTRA());
+                stmt.setInt(1,hd.getmADOCGIA());
                 stmt.setInt(2, hd.gettIENTHU());
                 stmt.execute();
             } catch (SQLException e) {
                 throw new ArithmeticException(e.getMessage());
             }
     }
-    public static boolean KTMaPhieuTra(int mAPHIEUTRA){
-        String sql ="select MAPHIEUTRA from PHIEUTRASACH WHERE MAPHIEUTRA=?";
-        try (PreparedStatement stm = conn.prepareStatement(sql)) {
-                
-                stm.setInt(1,mAPHIEUTRA);
-                ResultSet rs = stm.executeQuery();
-                return rs.next();
-                
-            } catch (SQLException e) {
-                throw new ArithmeticException(e.getMessage());
-            }
-
-        
-    }
+   
     public static boolean KIEMTRA(HoaDon hd){
-        String sql ="""
-                    SELECT DOCGIA.TIENNO
-                    FROM PHIEUTRASACH
-                    JOIN PHIEUMUONSACH ON PHIEUMUONSACH.MAPHIEUMUONSACH = PHIEUTRASACH.MAPHIEUMUONSACH
-                    JOIN DOCGIA ON PHIEUMUONSACH.MADOCGIA = DOCGIA.MADOCGIA
-                    WHERE PHIEUTRASACH.MAPHIEUTRA = ?""";
+        String sql ="SELECT TIENNO FROM DOCGIA WHERE MADOCGIA=?";
         try (PreparedStatement stm = conn.prepareStatement(sql)) {
                 
-                stm.setInt(1,hd.getmAPHIEUTRA());
+                stm.setInt(1,hd.getmADOCGIA());
                 ResultSet rs = stm.executeQuery();
                 if(rs.next()){
                     int tIENNO = rs.getInt("TIENNO");
@@ -61,5 +62,26 @@ public class HoaDonDAO {
                 throw new ArithmeticException(e.getMessage());
             }
 
+    }
+    public static List<HoaDon> getlistHOADON(){
+        String sql="select* from HOADON";
+        List<HoaDon> list=new ArrayList<>();
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
+             ResultSet rs = stm.executeQuery();
+             while(rs.next()){
+                 HoaDon A=new HoaDon();
+                 A.setmAHOADON(rs.getInt("MAHD"));
+                 A.setmADOCGIA(rs.getInt("MADOCGIA"));
+                 A.settIENTHU(rs.getInt("TIENTHU"));
+                 list.add(A);
+                 
+             }   
+                
+            } catch (SQLException e) {
+                throw new ArithmeticException(e.getMessage());
+            }
+        
+        return list;
+        
     }
 }
