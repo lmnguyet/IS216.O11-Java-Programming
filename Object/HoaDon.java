c34828b90d2bebdb3dc2161db3314973c71a9f4e
trungtin1808 <154118963+trungtin1808@users.noreply.github.com>
2023-12-21 08:18:52 +0700

    Create HoaDon.java

---
+++ Object/HoaDon.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objects;
+
+/**
+ *
+ * @author Admin
+ */
+import java.sql.Date;
+
+/**
+ *
+ * @author minh
+ */
+public class HoaDon  {
+   private int mAHOADON;
+   private int mADOCGIA;
+   private int tIENTHU;
+   
+   public HoaDon(){
+       
+   }
+   public HoaDon(int mAHOADON,int mADOCGIA,int tIENTHU){
+       this.mAHOADON=mAHOADON;
+       this.mADOCGIA=mADOCGIA;
+       this.tIENTHU=tIENTHU;
+   }
+   public int getmAHOADON(){
+       return mAHOADON;
+   }
+   public void setmAHOADON(int mAHOADON){
+       this.mAHOADON=mAHOADON;
+   }
+   public int getmADOCGIA(){
+       return mADOCGIA;
+   }
+   public void setmADOCGIA(int mADOCGIA){
+       this.mADOCGIA=mADOCGIA;
+   }
+   public int gettIENTHU(){
+       return tIENTHU;
+   }
+   public void settIENTHU(int tIENTHU){
+       this.tIENTHU=tIENTHU;
+   }
+}
